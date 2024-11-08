aa38fb3a24d35193fcf7ecb29aa4d379c53c0e9e
Arunkumar C <17872178+Takkaiscool@users.noreply.github.com>
2024-11-08 21:44:34 +0530

    added

--- src/test/java/Tests/RunnerClass.java
+++ src/test/java/Tests/RunnerClass.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/resources/Features",
         glue = "StepDefinitions",
         tags = "@smoke",
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "summary", "pretty", "html:TestReports/CucumberReport/cucumber.html", "json:target/cucumber-report/cucumber.json"})
 public class RunnerClass extends AbstractTestNGCucumberTests {
 
 }
